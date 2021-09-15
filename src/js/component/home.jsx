debc625f0ced6f9880a95cb00d782b6ebdf38a4e
YaraGomezSaiz <y.gomez.saiz@hotmail.com>
2021-09-15 12:15:49 +0000

    test

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,7 +7,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
+			<h1>Hello Yara!</h1>
 			<p>
 				<img src={rigoImage} />
 			</p>
