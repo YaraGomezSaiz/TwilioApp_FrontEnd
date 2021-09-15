1ad850d767be135e829cb610480d8e623eda2aed
YaraGomezSaiz <y.gomez.saiz@hotmail.com>
2021-09-15 13:00:07 +0000

    nav

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,61 +7,121 @@ import "../../styles/index.scss";
 
 //create your first component
 const Home = () => {
-	return (<div className="text-center mt-5">
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="#">
-					<img src="https://toppng.com/uploads/preview/twilio-logo-11609380242jwkoktksba.png" alt="" width="200"
-						height="100" />
-				</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="#">Home</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Link</a>
-						</li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
-								aria-expanded="false">
-								Dropdown
-							</a>
-							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-								<li><a class="dropdown-item" href="#">Action</a></li>
-								<li><a class="dropdown-item" href="#">Another action</a></li>
-								<li>
-									<hr class="dropdown-divider" />
-								</li>
-								<li><a class="dropdown-item" href="#">Something else here</a></li>
-							</ul>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-						</li>
-					</ul>
-					<form class="d-flex">
-						<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-						<button class="btn btn-outline-success" type="submit">Search</button>
-					</form>
+	return (
+		<div className="text-center mt-5">
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#">
+						<img
+							src="https://toppng.com/uploads/preview/twilio-logo-11609380242jwkoktksba.png"
+							alt=""
+							width="200"
+							height="100"
+						/>
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div
+						className="collapse navbar-collapse"
+						id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a
+									className="nav-link active"
+									aria-current="page"
+									href="#">
+									Home
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">
+									Link
+								</a>
+							</li>
+							<li className="nav-item dropdown">
+								<a
+									className="nav-link dropdown-toggle"
+									href="#"
+									id="navbarDropdown"
+									role="button"
+									data-bs-toggle="dropdown"
+									aria-expanded="false">
+									Dropdown
+								</a>
+								<ul
+									className="dropdown-menu"
+									aria-labelledby="navbarDropdown">
+									<li>
+										<a className="dropdown-item" href="#">
+											Action
+										</a>
+									</li>
+									<li>
+										<a className="dropdown-item" href="#">
+											Another action
+										</a>
+									</li>
+									<li>
+										<hr className="dropdown-divider" />
+									</li>
+									<li>
+										<a className="dropdown-item" href="#">
+											Something else here
+										</a>
+									</li>
+								</ul>
+							</li>
+							<li className="nav-item">
+								<a
+									className="nav-link disabled"
+									href="#"
+									tabIndex="-1"
+									aria-disabled="true">
+									Disabled
+								</a>
+							</li>
+						</ul>
+						<form className="d-flex">
+							<input
+								className="form-control me-2"
+								type="search"
+								placeholder="Search"
+								aria-label="Search"
+							/>
+							<button
+								className="btn btn-outline-success"
+								type="submit">
+								Search
+							</button>
+						</form>
+					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
 
-		<div class="jumbotron p-4 jumbotron-fluid">
-			<h1 class="display-4">Do you want to play?</h1>
-			<p class="lead">I know that you are really excited :) </p>
-			<hr class="my-4" />
-			<p>Press button to get your price</p>
-			<p class="lead">
-				<a class="btn btn-primary btn-lg" href="#" role="button">Play</a>
-			</p>
+			<div className="jumbotron p-4 jumbotron-fluid">
+				<h1 className="display-4">Do you want to play?</h1>
+				<p className="lead">I know that you are really excited :) </p>
+				<hr className="my-4" />
+				<p>Press button to get your price</p>
+				<p className="lead">
+					<a
+						className="btn btn-primary btn-lg"
+						href="#"
+						role="button">
+						Play
+					</a>
+				</p>
+			</div>
 		</div>
-	</div>)
+	);
 };
 
 export default Home;
