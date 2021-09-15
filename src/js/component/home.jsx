e3787f475dd4509661946da87c65ee19ff3bced7
YaraGomezSaiz <y.gomez.saiz@hotmail.com>
2021-09-15 12:57:43 +0000

    navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,67 @@
 import React from "react";
+//include bootstrap npm library into the bundle
+import "bootstrap/dist/css/bootstrap.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//include your index.scss file into the bundle
+import "../../styles/index.scss";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Yara!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+	return (<div className="text-center mt-5">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">
+					<img src="https://toppng.com/uploads/preview/twilio-logo-11609380242jwkoktksba.png" alt="" width="200"
+						height="100" />
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+								aria-expanded="false">
+								Dropdown
+							</a>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<li><a class="dropdown-item" href="#">Action</a></li>
+								<li><a class="dropdown-item" href="#">Another action</a></li>
+								<li>
+									<hr class="dropdown-divider" />
+								</li>
+								<li><a class="dropdown-item" href="#">Something else here</a></li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+						</li>
+					</ul>
+					<form class="d-flex">
+						<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+						<button class="btn btn-outline-success" type="submit">Search</button>
+					</form>
+				</div>
+			</div>
+		</nav>
+
+		<div class="jumbotron p-4 jumbotron-fluid">
+			<h1 class="display-4">Do you want to play?</h1>
+			<p class="lead">I know that you are really excited :) </p>
+			<hr class="my-4" />
+			<p>Press button to get your price</p>
+			<p class="lead">
+				<a class="btn btn-primary btn-lg" href="#" role="button">Play</a>
 			</p>
 		</div>
-	);
+	</div>)
 };
 
 export default Home;
